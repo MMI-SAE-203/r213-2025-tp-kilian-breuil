fd6ba6537d48b4db6835c656df88412169aa5430
kilian-breuil <150059078+kilian-breuil@users.noreply.github.com>
2025-02-07 08:56:31 +0100

    les 2 test fonctionne

--- src/js/backend.mjs
+++ src/js/backend.mjs
@@ -1,18 +1,29 @@
 import PocketBase from 'pocketbase';
 
-const db = new PocketBase('http://127.0.0.1:8090');
+const pb = new PocketBase('http://127.0.0.1:8090');
 
 export async function getOffres() {
     try {
-        let data = await db.collection('maison').getFullList({
+        let data = await pb.collection('maison').getFullList({
         });
         data = data.map((d) => {
-            d.field = db.files.getURL(d, d.field);
+            d.field = pb.files.getURL(d, d.field);
             return d;
         });
         return data;
     } catch (error) {
         console.log('Une erreur est survenue en lisant la liste des maisons', error);
         return [];
+    }
+}
+
+export async function getOffre(id) {
+    try {
+        let data = await pb.collection('maison').getOne(id);
+        data.imageUrl = pb.files.getURL(data, data.image);
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue en lisant la maison', error);
+        return null;
     }
 }
